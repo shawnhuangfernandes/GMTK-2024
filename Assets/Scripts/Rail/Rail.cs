3de0a3564c8252d8bec3001ad9d5278ee39bd620
S B <se.email.exe@gmail.com>
2024-08-17 14:39:27 -0500

    Add Rail prefab and script
    Add rail grinding support to CustomFirstPersonController.cs

---
+++ Assets/Scripts/Rail/Rail.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Dreamteck.Splines;
+
+/// <summary>
+/// A rail on which a CustomFirstPersonController can grind.
+/// </summary>
+[RequireComponent(typeof(SplineComputer))]
+public class Rail : MonoBehaviour
+{
+	[field: SerializeField()]
+	public SplineComputer spline { get; private set; }
+
+	private void Awake()
+	{
+		spline = GetComponent<SplineComputer>();
+	}
+}
--- Assets/Scripts/Temp/CustomFirstPersonController.cs
+++ Assets/Scripts/Temp/CustomFirstPersonController.cs
@@ -27,17 +27,29 @@ public class CustomFirstPersonController : MonoBehaviour
     [Tooltip("The camera XFRM to control")]
     public Transform cameraXFRM;
 
+    [Header("Rail Grinding")]
+    [Tooltip("The object at whose position this character should attach to rails while grinding.")]
+    public Dreamteck.Splines.SplineProjector railHandle;
+    public float minGrindSpeed = 5;
+    public float maxGrindSpeed = 15;
+    public float snapToRailLerpRate = 0.9f;
+
     private CharacterController characterController;
     private Vector3 velocity;
+    /// <summary> The movement mode this character should surrently be following. </summary>
+    [field: SerializeField()]
     public MotionState motionState { get; private set; }
     
     private float rotationX = 0;
+    private Rail? currentRail;
 
 	public enum MotionState {
         /// <summary> Move according to player input, and stick to the ground. </summary>
         Grounded,
         /// <summary> Move according to player input and the force of gravity. </summary>
         Airborne,
+        /// <summary> Move along the current rail until this character reaches the end or jumps off. </summary>
+        Grinding,
     }
 
 
@@ -51,19 +63,31 @@ public class CustomFirstPersonController : MonoBehaviour
     void Update()
     {
         HandleMovement();
+        InteractWithRails();
         // After all movement, decide how this character should respond to the ground.
         InteractWithGround();
         HandleCameraRotation();
     }
 
     void HandleMovement()
-    {
-        float speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
-        Vector3 moveDirection = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical");
-        moveDirection *= speed;
+	{
+		// If able, apply player movement input.
+		switch (motionState)
+		{
+            case MotionState.Grounded:
+            case MotionState.Airborne:
+		        float speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
+                Vector3 moveDirection = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical");
+                moveDirection *= speed;
 
-        // Applying movement to the velocity.
-        velocity = new Vector3(moveDirection.x, velocity.y, moveDirection.z);
+                // Applying movement to the velocity.
+                velocity = new Vector3(moveDirection.x, velocity.y, moveDirection.z);
+                break;
+
+            case MotionState.Grinding:
+                // No directional control while grinding.
+                break;
+        }
 
         switch(motionState)
 		{
@@ -82,11 +106,59 @@ public class CustomFirstPersonController : MonoBehaviour
                 // Apply gravity
                 AddForce(Physics.gravity, ForceMode.Acceleration);
                 break;
+
+            case MotionState.Grinding:
+                // First, apply gravity.
+                AddForce(Physics.gravity, ForceMode.Acceleration);
+
+                // Then, align velocity to the current rail.
+                velocity = velocity.magnitude * railHandle.result.forward;
+                break;
 		}
 
+        // Calculate the upcoming displacement based on this character's current velocity.
+        Vector3 displacement = velocity * Time.deltaTime;
+
+        // Nudge this character's position.
+        switch (motionState)
+		{
+            case MotionState.Grinding:
+                // Snap to the current rail.
+                float nudgeFactor = (1f - Mathf.Pow(1f-snapToRailLerpRate, Time.deltaTime));
+                displacement += (railHandle.result.position - railHandle.transform.position) * nudgeFactor;
+                break;
+        }
+
         // Finally, move this character.
-        characterController.Move(velocity * Time.deltaTime);
+        characterController.Move(displacement);
     }
+
+    /// <summary> If able, search for nearby rails and start grinding. </summary>
+    private void InteractWithRails()
+	{
+		switch (motionState)
+		{
+            case MotionState.Grounded:
+            case MotionState.Airborne:
+                foreach (Collider otherCollider in Physics.OverlapSphere(transform.position, 1.5f))
+                {
+                    Rail otherRail = GetComponent<Rail>();
+                    if (otherRail == null)
+                        continue;
+
+                    // Found a rail! Snap to it.
+                    motionState = MotionState.Grinding;
+                    currentRail = otherRail;
+                    railHandle.spline = otherRail.spline;
+                    break;
+                }
+                break;
+
+            case MotionState.Grinding:
+                // There's no need to interact with nearby rails if this character is already grinding.
+                break;
+        }
+	}
 
     private void InteractWithGround()
     {
