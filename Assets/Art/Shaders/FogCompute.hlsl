33eeb32420dd628edc9ea5c5c8ab071bd551051d
ryandeardorff <ryandeardorff@users.noreply.github.com>
2024-08-17 00:16:38 -0700

    Beginning of art work
    - Added pieces to ruins kit
    - Added more grass to foliage
    - Added wavyness to foliage
    - Added rocks for scattering
    - Added Tint decals
    - Added new materials
    - Tweaked post processing
    - realized how stupid big this commit was my bad.

---
+++ Assets/Art/Shaders/FogCompute.hlsl
@@ -0,0 +1,20 @@
+#define FOG_EXP2
+
+void ComputeFogIntensity_float(float fogFactor, out float o)
+{
+    float fogIntensity = 0.0h;
+#if defined(FOG_LINEAR) || defined(FOG_EXP) || defined(FOG_EXP2)
+#if defined(FOG_EXP)
+    // factor = exp(-density*z)
+    // fogFactor = density*z compute at vertex
+    fogIntensity = saturate(exp2(-fogFactor));
+#elif defined(FOG_EXP2)
+    // factor = exp(-(density*z)^2)
+    // fogFactor = density*z compute at vertex
+    fogIntensity = saturate(exp2(-fogFactor * fogFactor));
+#elif defined(FOG_LINEAR)
+    fogIntensity = fogFactor;
+#endif
+#endif
+    o = fogIntensity;
+}
