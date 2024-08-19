a4f860924169b91f005470315f7df36d1f96240b
S B <se.email.exe@gmail.com>
2024-08-18 22:30:50 -0500

    Add footstep sounds
    Add a audioSwitchToUse field to PlaySoundWwise.cs

--- Assets/Scripts/Temp/PlaySoundWwise.cs
+++ Assets/Scripts/Temp/PlaySoundWwise.cs
@@ -3,10 +3,15 @@ using UnityEngine;
 public class PlaySoundWwise : MonoBehaviour
 {
     [Tooltip("The audio event to play for this sound")]
-    [SerializeField] private AK.Wwise.Event audioEventToPlay;
+    public AK.Wwise.Event audioEventToPlay;
+    [Tooltip("The audio switch to use for this sound, if any")]
+    public AK.Wwise.Switch audioSwitchToUse;
 
     public void PlayAudioEvent()
     {
+        if(audioSwitchToUse != null)
+            audioSwitchToUse.SetValue(gameObject);
+
         audioEventToPlay.Post(gameObject);
     }
 }
